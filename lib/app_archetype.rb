e7c5e60097fadaca56a7f29a139c063527ce0103
andrewbigger <abigger@zendesk.com>
2020-01-08 21:09:21 +1100

    Add CLI command

--- lib/app_archetype.rb
+++ lib/app_archetype.rb
@@ -1,3 +1,9 @@
+require 'app_archetype/version'
+require 'app_archetype/file'
+require 'app_archetype/template'
+require 'app_archetype/plan'
+require 'app_archetype/renderer'
+
 # AppArchetype is the namespace for app_archetype
 module AppArchetype
 end
--- lib/app_archetype/cli.rb
+++ lib/app_archetype/cli.rb
@@ -5,6 +5,32 @@ module AppArchetype
   module CLI
     # CLI Actions
     module Commands
+      ##
+      # Template render command
+      #
+      def self.render(
+        cli,
+        template_path,
+        destination_path,
+        manifest_path,
+        overwrite = false,
+        vars = []
+      )
+        variables = Hashie::Mash.new
+
+        vars.each do |var|
+          pair = var.split(':')
+          variables[pair.shift] = pair.shift
+        end
+
+        # TODO: load and merge manifest variables
+        template = Template.new(template_path)
+        
+        plan = Plan.new(template, destination_path, variables)
+        plan.devise
+
+        Renderer.new(plan, variables, overwrite).render
+      end
     end
 
     # CLI Helpers
