42f02b964c3c135c5bf14b6a48abf8e81846d698
andrewbigger <abigger@zendesk.com>
2020-01-25 11:24:49 +1100

    Add random string generator

--- lib/app_archetype/variables.rb
+++ lib/app_archetype/variables.rb
@@ -33,5 +33,16 @@ module AppArchetype
     def dot
       ''
     end
+
+    ##
+    # rand
+    # 
+    # generates a randoom string at specified length
+    #
+    # @param length - size of string
+    def rand(length=256)
+      key_set = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a
+      (0...length).map { key_set[Random.rand(0..key_set.length)] }.join
+    end
   end
 end
