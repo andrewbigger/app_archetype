42df1546bb08b949aa8e558864c7c4001e11878a
Andrew Bigger <andrewbigger@users.noreply.github.com>
2021-10-29 16:37:00 +1100

    Remove render method from app archetype module

--- lib/app_archetype.rb
+++ lib/app_archetype.rb
@@ -7,41 +7,8 @@ require 'app_archetype/template_manager'
 require 'app_archetype/renderer'
 require 'app_archetype/generators'
 
-require 'app_archetype/cli/prompts'
-
 require 'app_archetype/version'
 
 # AppArchetype is the namespace for app_archetype
 module AppArchetype
-  def self.render(
-    name,
-    templates_dir,
-    destination_path: Dir.pwd,
-    overwrite: true,
-    variables: []
-  )
-    manifest_file = File.join(templates_dir, name, 'manifest.json')
-
-    manifest = AppArchetype::Template::Manifest.new_from_file(manifest_file)
-
-    template = manifest.template
-    template.load
-
-    variables.each { |var| manifest.variables.add(var) }
-
-    manifest.variables.all.each do |var|
-      value = AppArchetype::CLI::Prompts.variable_prompt_for(var)
-      var.set!(value)
-    end
-
-    plan = AppArchetype::Template::Plan.new(
-      template,
-      manifest.variables,
-      destination_path: destination_path,
-      overwrite: overwrite
-    )
-
-    plan.devise
-    plan.execute
-  end
 end
